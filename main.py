00e8d07a779bc6ee3e83607d8ab78c1cef8ed45c
amenoyoya <mtyswe3@gmail.com>
2019-06-12 00:10:19 +0900

    追加｜変数名判定クラス等

--- main.py
+++ main.py
@@ -1,29 +1,71 @@
 # 関数の引数の数を取得するためにsignatureをimport
 from inspect import signature
+import re
 
-def RPN(exp, op):
-    ''' 逆ポーランド記法を計算する関数
-    params:
-        exp list: 逆ポーランド記法プログラムの配列（el. [1, 2, '+'] => 1 + 2）
-        op dict: 演算子,変数の定義（el. {'+': (lambda x, y: x + y), 'ten': 10}）
-    '''
-    stack = []
-    for e in exp:
-        f = op.get(e)
-        if f is None:
-            # 演算子でも変数でもない場合はstack
-            stack.append(e)
-        elif callable(f):
-            # 演算子なら演算実行
-            argc = len(signature(f).parameters) # 関数の引数の数を取得
-            res = f(*stack[-argc:]) # stackの後ろから引数を取得し、関数実行
-            stack = stack[:-argc] # 引数分をstackから削除
-            if res is not None:
-                stack.append(res) # 関数の戻り値をstack
-        else:
-            # 変数の値をstack
-            stack.append(f)
-    return stack
+class RPN:
+    ''' 逆ポーランド記法計算クラス '''
+
+    class Variable:
+        ''' 変数名として使える文字列か判定するクラス '''
+        class NameError(Exception):
+            def __str__(self):
+                return 'Invalid character specified for variable name.'
+        
+        # 変数名には[アルファベット, 数字, _, .]のみ使用可能
+        pattern = re.compile(r'^[a-zA-Z0-9_\.]+$')
+        
+        def __init__(self, variable_name):
+            if Variable.pattern.match(variable_name) is None:
+                raise Variable.NameError()
+            self.name = variable_name
+        
+        def __str__(self):
+            return self.name
+
+    class Value:
+        ''' 値クラス '''
+        NUMERIC = 0 # 数値
+        STRING = 1 # 文字列
+        VARIABLE = 2 # 変数
+        CHUNK = 3 # 遅延評価式
+
+        def __init__(self, value, _type):
+            self.value = str(RPN.Variable(value)) if _type == VARIABLE else str(value)
+            self.type = _type
+        
+        def __str__(self):
+            return self.value
+
+    def __init__(self, operators):
+        ''' コンストラクタ
+        params:
+            operators dict: 演算子,変数の定義（el. {'+': (lambda x, y: x + y), 'ten': 10}）
+        '''
+        self.operators = operators
+
+    def eval(self, exp):
+        ''' 逆ポーランド記法を計算する関数
+        params:
+            exp list: 逆ポーランド記法プログラムの配列（el. [1, 2, '+'] => 1 + 2）
+        '''
+        stack = []
+        op = self.operators
+        for e in exp:
+            f = op.get(e)
+            if f is None:
+                # 演算子でも変数でもない場合はstack
+                stack.append(e)
+            elif callable(f):
+                # 演算子なら演算実行
+                argc = len(signature(f).parameters) # 関数の引数の数を取得
+                res = f(*stack[-argc:]) # stackの後ろから引数を取得し、関数実行
+                stack = stack[:-argc] # 引数分をstackから削除
+                if res is not None:
+                    stack.append(res) # 関数の戻り値をstack
+            else:
+                # 変数の値をstack
+                stack.append(f)
+        return stack
 
 
 if __name__ == "__main__":
@@ -50,4 +92,5 @@ if __name__ == "__main__":
     expression = [1, 2, '+', 3, '*', 4, '-', 'ten', '*', 'hundred', '/', 'pi', '+']
 
     # 演算実行
-    print(RPN(expression, operators))
+    rpn = RPN(operators)
+    print(rpn.eval(expression))
