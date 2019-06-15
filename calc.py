fb1946e2d0be90b7c592dfc324a4c9c7efe84db0
amenoyoya <mtyswe3@gmail.com>
2019-06-15 13:37:43 +0900

    実装｜RPNクラス化

--- calc.py
+++ calc.py
@@ -1,23 +1,35 @@
 # encoding: utf-8
 
-from RPN import Value, eval_rpn_variable, pn2rpn
+from rpn import RPN, Value, Variable
 
-op = {
-    '+': lambda x, y: Value(x.value + y.value),
-    '-': lambda x, y: Value(x.value - y.value),
-    '*': lambda x, y: Value(x.value * y.value),
-    '/': lambda x, y: Value(x.value / y.value),
-}
-
-rpn = pn2rpn(op, {
-    '+': {
-        '*': {
-            '-': [123, 23],
-            '/': [50, 2]
-        },
-        '-': [550, 50]
+# 日本語四則演算子
+class Calc(RPN):
+    operators = {
+        '足す': lambda x, y: Value(x.value + y.value),
+        '引く': lambda x, y: Value(x.value - y.value),
+        '掛ける': lambda x, y: Value(x.value * y.value),
+        '割る': lambda x, y: Value(x.value / y.value),
     }
+
+# ポーランド記法から逆ポーランド記法構築
+## (割る (掛ける (足す 1 2) (引く 5 10)) 3)
+## => [1, 2, '+', 5, 10, '-', '*', 3, '/']
+rpn = Calc.build({
+    '割る': [
+        {
+            '掛ける': {
+                '足す': [1, 2],
+                '引く': [5, 10],
+            }
+        }, 3
+    ]
 })
 
-print([e if type(e) == str else e.value for e in rpn])
-print([e.value for e in eval_rpn_variable(rpn, op)])
+print(Calc.explain(rpn))
+
+# 逆ポーランド記法を計算
+## [1, 2, '+', 5, 10, '-', '*', 3, '/']
+## => {(1 + 2) * (5 -10)} / 3
+## => -5.0
+ans = Calc.eval(rpn)
+print(Calc.explain(ans))
